2ef73e6adff4b7aa152780291317550c3e23064f
johnwav <102820657+johnwav@users.noreply.github.com>
2023-02-05 23:01:50 +0100

    Implemented QR component

--- QRCodeComponent/@latest/src/App.jsx
+++ QRCodeComponent/@latest/src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import qr from './assets/image-qr-code.png'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <div className='Container'>
+        <img className='qr-image' src={qr}></img>
+        <div className='Title'>
+          Improve your front-end skills by building projects
+        </div>
+        <div className='Description'>
+          Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+        </div>
+      </div>
     </div>
   )
 }
